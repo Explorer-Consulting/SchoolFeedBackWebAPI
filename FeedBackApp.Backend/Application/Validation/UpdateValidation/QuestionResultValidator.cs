5be6d1bc16053336727deff1d28ec9809a879f81
KincsoV <kincso.gabriella.varga@stud.ubbcluj.ro>
2025-08-27 13:08:28 +0300

    modified validate to match with  frontend

--- FeedBackApp.Backend/Application/Validation/UpdateValidation/QuestionResultValidator.cs
+++ FeedBackApp.Backend/Application/Validation/UpdateValidation/QuestionResultValidator.cs
@@ -29,7 +29,7 @@ namespace Application.Validation.UpdateValidation
                     switch (template.Type)
                     {
                         case QuestionType.MultinomialSingleChoice:
-                            if (!int.TryParse(answer, out int singleChoice) || singleChoice < 0 || singleChoice >= template.AnswerOptions.Count)
+                            if (!int.TryParse(answer, out int singleChoice) || singleChoice < 1 || singleChoice > template.AnswerOptions.Count)
                                 context.AddFailure("Answer", $"Answer must be a number between 0 and {template.AnswerOptions.Count - 1} for '{template.Question}-{template.Id}'.");
                             break;
 
@@ -37,7 +37,7 @@ namespace Application.Validation.UpdateValidation
                             var parts = answer.Split('-', StringSplitOptions.RemoveEmptyEntries);
                             foreach (var part in parts)
                             {
-                                if (!int.TryParse(part, out int choice) || choice < 0 || choice >= template.AnswerOptions.Count)
+                                if (!int.TryParse(part, out int choice) || choice < 1 || choice > template.AnswerOptions.Count)
                                 {
                                     context.AddFailure("Answer", $"Answer index {part} is invalid for '{template.Question}-{template.Id}'.");
                                 }
@@ -45,14 +45,14 @@ namespace Application.Validation.UpdateValidation
                             break;
 
                         case QuestionType.LikertScaleOneToFive:
-                            if (!int.TryParse(answer, out int scale) || scale < 0 || scale > 5)
+                            if (!int.TryParse(answer, out int scale) || scale < 1 || scale > 5)
                                 context.AddFailure("Answer", $"Answer must be a number between 0 and 5 for '{template.Question}-{template.Id}'.");
                             break;
 
                         case QuestionType.MultiNomialSingleChoiceOther:
-                            if (int.TryParse(answer, out int choice2))
+                            if (int.TryParse(answer, out int singleChoice2))
                             {
-                                if (choice2 < 0 || choice2 >= template.AnswerOptions.Count)
+                                if (singleChoice2 < 1 || singleChoice2 > template.AnswerOptions.Count)
                                 {
                                     context.AddFailure("Answer", $"Answer must be a number between 0 and {template.AnswerOptions.Count - 1} for '{template.Question}-{template.Id}'.");
                                 }
