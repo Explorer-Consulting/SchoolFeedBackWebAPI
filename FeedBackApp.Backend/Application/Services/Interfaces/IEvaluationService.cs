9d4ed0e01e2fe1133a8449e3146d11d825fb4c79
KisMiska <galicaervin@gmail.com>
2025-08-27 11:22:43 +0300

    duplicate dto fix and azure funct

--- FeedBackApp.Backend/Application/Services/Interfaces/IEvaluationService.cs
+++ FeedBackApp.Backend/Application/Services/Interfaces/IEvaluationService.cs
@@ -5,5 +5,7 @@ namespace Application.Services.Interfaces
     public interface IEvaluationService
     {
         public Task<UpdateResponseDTO> UpdateQuestionnaire(string id, UpdateQuestionnaireDTO dto);
+
+        public Task<SubmitResponseDTO> SubmitQuestionnaire(string id, SubmitQuestionnaireDTO dto);
     }
 }
