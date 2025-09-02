865882f931abaef2790155b452448d3a9e7ff748
SzLRichard <95868564+SzLRichard@users.noreply.github.com>
2025-09-02 11:29:28 +0300

    Fixed dumb mistake

--- FeedBackApp.Backend/FeedBackApp.Core/Repositories/IEvaluationRepository.cs
+++ FeedBackApp.Backend/FeedBackApp.Core/Repositories/IEvaluationRepository.cs
@@ -7,8 +7,8 @@ namespace FeedBackApp.Core.Repositories
     {
         Task<bool> UpdateOrSubmitQuestionnaire(Questionnaire newQuestionnaire, Questionnaire oldQuestionnaire);
         Task<Questionnaire?> GetQuestionnaireByIdAsync(string id);
-        Task<Questionnaire?> GetQuestionnaireByIdAsNoTrackingAsync(string id);
-        Task<QuestionnaireTemplate?> GetQuestionnaresByIdAsNoTrackingAsync(string surevyId);
+        Task<Questionnaire?> GetQuestionnaresByIdAsNoTrackingAsync(string id);
+        Task<QuestionnaireTemplate?> GetQuestionTemplateBySurveyIdAsync(string surevyId);
         Task<DateTime?> GetEndDateBySurveyId(string surveyId);
 
     }
