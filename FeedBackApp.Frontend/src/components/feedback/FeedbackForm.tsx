b7f26ca6ad4fbdbfad66a786bd598109f723cb49
Lorincz-Gabriella <lorinczgabriella32@gmail.com>
2025-09-02 11:32:30 +0300

    question lenght fix

--- FeedBackApp.Frontend/src/components/feedback/FeedbackForm.tsx
+++ FeedBackApp.Frontend/src/components/feedback/FeedbackForm.tsx
@@ -105,6 +105,7 @@ export function FeedbackForm({
 
   const id = currentEvaluation?.id;
   const likertValues = ["1", "2", "3", "4", "5"];
+
   const qValues = useMemo(
     () => Array.from({ length: 17 }, (_, i) => form[`q${i}` as keyof FeedbackFormState] as string),
     [form]
@@ -151,11 +152,11 @@ export function FeedbackForm({
       toast("Kérjük, válassz legalább egy lehetőséget a 21. kérdésnél.");
       return;
     }
-    if (form.q21.length < 50) {
+    if (form.q21.length < 20) {
       toast("A 22. kérdésnél a válasznak legalább 50 karakternek kell lennie.");
       return;
     }
-    if (form.q22.length < 50) {
+    if (form.q22.length < 20) {
       toast("A 23. kérdésnél a válasznak legalább 50 karakternek kell lennie.");
       return;
     }
--- FeedBackApp.Frontend/src/utils/feedback-form.state.ts
+++ FeedBackApp.Frontend/src/utils/feedback-form.state.ts
@@ -1,19 +1,58 @@
 export interface FeedbackFormState {
-   [key: `q${number}`]: string | string[];
-  q0: string;  q1: string;  q2: string;  q3: string;  q4: string;
-  q5: string;  q6: string;  q7: string;  q8: string;  q9: string;
-  q10: string; q11: string; q12: string; q13: string; q14: string;
-  q15: string; q16: string; q17: string; q18: string;
+  [key: `q${number}`]: string | string[];
+  q0: string;
+  q1: string;
+  q2: string;
+  q3: string;
+  q4: string;
+  q5: string;
+  q6: string;
+  q7: string;
+  q8: string;
+  q9: string;
+  q10: string;
+  q11: string;
+  q12: string;
+  q13: string;
+  q14: string;
+  q15: string;
+  q16: string;
+  q17: string;
+  q18: string;
   q19: string[];
-  q20: string[]; 
-  q21: string; q22: string; q23: string; q24: string; q25: string;
+  q20: string[];
+  q21: string;
+  q22: string;
+  q23: string;
+  q24: string;
+  q25: string;
 }
 
 export const initialFeedbackForm: FeedbackFormState = {
-  q0:"", q1:"", q2:"", q3:"", q4:"",
-  q5:"", q6:"", q7:"", q8:"", q9:"",
-  q10:"", q11:"", q12:"", q13:"", q14:"",
-  q15:"", q16:"", q17:"", q18:"",
-  q19:[], q20:[],
-  q21:"", q22:"", q23:"", q24:"", q25:""
+  q0: "",
+  q1: "",
+  q2: "",
+  q3: "",
+  q4: "",
+  q5: "",
+  q6: "",
+  q7: "",
+  q8: "",
+  q9: "",
+  q10: "",
+  q11: "",
+  q12: "",
+  q13: "",
+  q14: "",
+  q15: "",
+  q16: "",
+  q17: "",
+  q18: "",
+  q19: [],
+  q20: [],
+  q21: "",
+  q22: "",
+  q23: "",
+  q24: "",
+  q25: ""
 };
