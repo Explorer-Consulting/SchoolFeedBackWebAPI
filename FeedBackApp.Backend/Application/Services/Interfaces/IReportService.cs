76deecd724a7e2f435a4bd309e2c5a70658213e8
KincsoV <kincso.gabriella.varga@stud.ubbcluj.ro>
2025-09-08 12:15:27 +0300

    creating the corresponding azure function endpoint for report compilation and the regarding service layer interface and implementation.
    further modifications are awaitable.
    
    # Conflicts:
    #	FeedBackApp.Backend/Application/Services/Interfaces/IReportService.cs
    #	FeedBackApp.Backend/Application/Services/ReportService.cs

---
+++ FeedBackApp.Backend/Application/Services/Interfaces/IReportService.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Services.Interfaces
+{
+    public interface IReportService
+    {
+        Task CompileAndStore(string id);
+    }
+}
---
+++ FeedBackApp.Backend/Application/Services/ReportService.cs
@@ -0,0 +1,18 @@
+﻿using Application.Services.Interfaces;
+using FeedBackApp.Core.Repositories;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Services
+{
+    public class ReportService(IReportRepository repository, ILogger<ReportService> logger) : IReportService
+    {
+        private readonly IReportRepository _repository = repository;
+        private readonly ILogger<ReportService> _logger = logger;
+
+        public async Task CompileAndStore(string id)
+        {
+            await _repository.CompileAndStoreEvaluationReports(id);
+            _logger.LogInformation("Compilation of reports in ReportService.....");
+        }
+    }
+}
