66f428d9510856b67cd6f4ca0482ca4baf31ac4c
Antal-Teofil <antalteofil.githubclient@gmail.com>
2025-10-18 02:42:46 +0300

    feat(commit-validation): commit validation is tested from UI

--- FeedBackApp.Backend/AzureFunctionsAPI/Program.cs
+++ FeedBackApp.Backend/AzureFunctionsAPI/Program.cs
@@ -22,6 +22,7 @@ using QuestPDF.Infrastructure;
 using System.Text.Json;
 
 // testing commit validation finally works
+// testing commit validation from user interface
 QuestPDF.Settings.License = LicenseType.Community;
 
 var host = new HostBuilder()
