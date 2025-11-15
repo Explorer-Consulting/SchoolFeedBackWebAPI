1642493e85f34ccedeab0175de43af51c26733bc
Antal-Teofil <antalteofil.githubclient@gmail.com>
2025-11-15 17:27:58 +0200

    refactor(questionnaires): refactoring questionnaire structure

---
+++ FeedBackApp.Backend/FeedBackApp.Core/DomainModels/QuestionnaireTemplateModels/QuestionnaireTemplate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ULID = NUlid.Ulid;
+
+namespace FeedBackApp.Core.DomainModels.QuestionnaireTemplateModels
+{
+    public sealed class QuestionnaireTemplate
+    {
+        public required QuestionnaireTemplateAttributeHeader Header { get; init; }
+
+    }
+}
