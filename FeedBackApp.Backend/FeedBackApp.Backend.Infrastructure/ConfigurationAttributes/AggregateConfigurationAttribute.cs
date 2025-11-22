fda9db9d2da1a532385d1043ffe21a7ce65beea9
Antal-Teofil <antalteofil.githubclient@gmail.com>
2025-11-22 02:36:43 +0200

    fix(naming): minor modification in naming

--- FeedBackApp.Backend/FeedBackApp.Backend.Infrastructure/ConfigurationAttributes/AggregateConfigurationAttribute.cs
+++ FeedBackApp.Backend/FeedBackApp.Backend.Infrastructure/ConfigurationAttributes/AggregateConfigurationAttribute.cs
@@ -9,7 +9,7 @@ namespace Infrastructure.ConfigurationAttributes
     [AttributeUsage(AttributeTargets.Class)]
     public sealed class AggregateConfigurationAttribute(string? ContainerName = default, string? Description = default) : Attribute
     {
-        public string? ContainerNamer { get; } = ContainerName;
+        public string? ContainerName { get; } = ContainerName;
         public string? Description { get; } = Description;
 
     }
