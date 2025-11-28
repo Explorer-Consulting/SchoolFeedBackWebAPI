84ab4de1b99d29ab92b21828c4b472da715e28fe
Antal-Teofil <antalteofil.githubclient@gmail.com>
2025-11-28 14:36:24 +0200

    refactor: introduce aggregate service

---
+++ FeedBackApp.Backend/ApplicationLayer/Interfaces/IAggregateService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLayer.Interfaces
+{
+    public interface IAggregateService<TAggregateDTO> where TAggregateDTO : class, IAggregateDTORoot
+    {
+        Task ConstructAggreateInstanceAsync(Action<TAggregateDTO> configure);
+        Task DeleteAggregateAsync(string aggregateId);
+        Task UpdateAggregateAsync(string aggregateId, Action<TAggregateDTO> configure);
+        Task<TAggregateDTO?> RetrieveAggregateAsync(Expression<Func<TAggregateDTO, bool>> predicate);
+        IAsyncEnumerable<TAggregateDTO> RetrieveAllAggregatesAsync(Expression<Func<TAggregateDTO, bool>>? predicate = null);
+    }
+}
---
+++ FeedBackApp.Backend/ApplicationLayer/Services/QuestionnaireTemplateService.cs
@@ -0,0 +1,39 @@
+﻿using ApplicationLayer.DataTransferObjects;
+using ApplicationLayer.Interfaces;
+using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLayer.Services
+{
+    public sealed class QuestionnaireTemplateService(IQuestionnaireTemplateAggregateRepository aggregateRepository) : IAggregateService<QuestionnaireTemplateDTO>
+    {
+        public Task ConstructAggreateInstanceAsync(Action<QuestionnaireTemplateDTO> configure)
+        {
+            aggregateRepository.ConstructAggregateInstance(configure);
+        }
+
+        public Task DeleteAggregateAsync(string aggregateId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<QuestionnaireTemplateDTO?> RetrieveAggregateAsync(System.Linq.Expressions.Expression<Func<QuestionnaireTemplateDTO, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncEnumerable<QuestionnaireTemplateDTO> RetrieveAllAggregatesAsync(System.Linq.Expressions.Expression<Func<QuestionnaireTemplateDTO, bool>>? predicate = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAggregateAsync(string aggregateId, Action<QuestionnaireTemplateDTO> configure)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
