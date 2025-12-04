d128ac52e9445d382fee665899e7f2f311b7b3c1
Sándor Kolumbán <sandor.kolumban@mirango.hu>
2025-12-04 08:01:04 +0200

    Add https to dev server.

--- FeedBackApp.Frontend/vite.config.ts
+++ FeedBackApp.Frontend/vite.config.ts
@@ -1,29 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-import devcert from "devcert";
+import mkcert from 'vite-plugin-mkcert'
 
-export default defineConfig(async ({ mode }) => {
-  const ssl = await devcert.certificateFor('localhost');
 
-  return {
-    server: {
-      host: "::",
-      port: 8080,
-      https: {
-        key: ssl.key,
-        cert: ssl.cert,
-      },
-    },
-    plugins: [
-      react(),
-      mode === "development" && componentTagger(),
-    ].filter(Boolean),
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
-    },
-  };
+export default defineConfig({
+  plugins: [ mkcert() ]
 });
