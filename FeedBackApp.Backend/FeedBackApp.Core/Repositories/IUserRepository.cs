bd7133d90fb4c11d79ea95d18e699e02c49e3bc2
Antal-Teofil <antalteofil.githubclient@gmail.com>
2025-12-28 16:56:19 +0200

    feature(users): adding interfaces for User handling

---
+++ FeedBackApp.Backend/FeedBackApp.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using FeedBackApp.Core.Model.UserIdentityModels;
+using NUlid;
+
+namespace FeedBackApp.Core.Repositories
+{
+    public interface IUserRepository
+    {
+        public Task UpsertUser(User entity);
+        public Task RemoveUser(Ulid userId);
+        public Task<User> RetrieveUser(Ulid userId);
+        public Task RemoveAllUsers(IEnumerable<Ulid> userIds);
+    }
+}
