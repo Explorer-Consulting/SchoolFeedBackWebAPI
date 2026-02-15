3ab3a36d52f777b32d4c8c6fe917fc2538768cb6
nfdoro <nfdoro@gmail.com>
2026-02-15 23:48:22 +0200

    feature: finished qr code link generation and store them in local storage, and correted preview page

---
+++ FeedBackApp.Frontend/src/models/SelfSignInLink.ts
@@ -0,0 +1,9 @@
+export type SelfSignInLink = {
+  id: string;
+  templateId: string;
+  templateTitle: string;
+  url: string;
+  expiresAt: string;
+  createdAt: string;
+  expirationMinutes: number;
+}
